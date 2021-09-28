b4b3393afbc8a81687cd216a2252251fc093b29a
Ram Cheekoti <ramcheekoti.hyd@gmail.com>
2021-09-28 08:11:36 -0700

    added and created ec2 instance successfully

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,11 @@
-provider "aws"
-{
+provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA6IO57UG4BQHYZT6N"
-  secret_key = "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"
-
+  access_key = "AKIA6IO57UG4BIERGBG3"
+  secret_key = "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"
+  # user for above credentials is ramch
 }
 
-resource "aws_instance" "myec2instance"
-{
+resource "aws_instance" "myec2instance" {
   # you need to look for arguments section of aws instance
   ami = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
