472f58fb63a06b1a924b8ca1dd80ce2ad1f707e7
Ram Cheekoti <ramcheekoti.hyd@gmail.com>
2021-09-29 07:27:02 -0700

    added the droplet

---
+++ droplet.tf
@@ -0,0 +1,11 @@
+provider "digitalocean" {
+  token = "6e47b74478bbd0f1353e2185b2820c279243cf47dfe4659352b3dcfdc8477394"
+
+}
+
+resource "digitalocean_droplet" "mydroplet" {
+  image  = "ubuntu-18-04-x64"
+  name   = "web-1"
+  region = "nyc1"
+  size   = "s-1vcpu-1gb"
+}
