d9a10e6335410d59f87383d810be4963900a1582
Ram Cheekoti <ramcheekoti.hyd@gmail.com>
2021-10-04 09:33:51 -0700

    added the files

--- attribute3.tf
+++ attribute3.tf
@@ -1,4 +1,5 @@
 /*
+
 provider "aws" {
   region = "us-east-1"
   profile = "default"
@@ -13,12 +14,26 @@ resource "aws_instance" "myec2" {
 
   ami = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
+  #security_groups = [allow_tls]
 }
 
 resource "aws_eip_association" "eip_assoc" {
 
   instance_id = aws_instance.myec2.id
   allocation_id = aws_eip.lb.id
+
+}
+
+resource "aws_security_group" "allow_tls" {
+
+  name = "allow_tls"
+
+  ingress {
+    from_port = 443
+    protocol  = "tcp"
+    to_port   = 443
+    cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+  }
 
 }
 
