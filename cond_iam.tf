0097399bee6213fcad33b1a257653248a520d32c
Ram Cheekoti <ramcheekoti.hyd@gmail.com>
2021-10-06 05:16:25 -0700

    added the files

---
+++ cond_iam.tf
@@ -0,0 +1,20 @@
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+
+}
+variable "istest" {
+
+}
+resource "aws_iam_user" "iamuser" {
+  name = "gaurav"
+  path = "/system/"
+  count = var.istest == true ? 3 : 0
+}
+
+resource "aws_iam_user" "iamuser1" {
+  name = "rajat"
+  path = "/system/"
+  count = var.istest == false ? 1 : 0
+}
